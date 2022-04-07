c0a57457170d3b10fb9a9741a0ae38d2d558065a
nuzril <muhammadnuzril2325@gmail.com>
2022-04-07 13:47:02 +0700

    portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    container:{
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors:{
+        primary: '#ec4899',
+        secondary: '#cbd5e1',
+        dark: '#0f172a',
+      },
+      screens:{
+        '2xl':'1320px',
+      }
+    },
+  },
+  plugins: [],
+}
